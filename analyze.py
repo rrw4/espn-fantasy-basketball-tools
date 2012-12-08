b43fb1e80f908d7c006199d55c72de525d70d6fc
Roger Wang <r.rj.wang@gmail.com>
2012-12-08 18:20:37 -0500

    build player ratings

--- analyze.py
+++ analyze.py
@@ -3,12 +3,15 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
-
 league_id = sys.argv[1]
+
 base_rosters_url = 'http://games.espn.go.com/fba/leaguerosters?leagueId='
 base_standings_url = 'http://games.espn.go.com/fba/standings?leagueId=47525&seasonId=2013'
 base_scoreboard_url = 'http://games.espn.go.com/fba/scoreboard?leagueId=47525&seasonId=2013'
+base_player_ratings_url_1 = 'http://games.espn.go.com/fba/playerrater?leagueId='
+base_player_ratings_url_2 = '&teamId=1&startIndex='
 
+player_ratings = {}
 
 def build_rosters_url(league_id):
     rosters_url = base_rosters_url + str(league_id)
@@ -19,6 +22,23 @@ def curl(url):
     html = response.read()
     return BeautifulSoup(html)
 
+def build_player_ratings_url(league_id, start_index):
+    player_ratings_url = base_player_ratings_url_1 + str(league_id) + base_player_ratings_url_2 + str(start_index)
+    return player_ratings_url
+
+def build_player_ratings(num_players):
+    for start_index in range(0, num_players, 50):
+        player_ratings_url = build_player_ratings_url(league_id, start_index)
+        player_ratings_dom = curl(player_ratings_url)
+        players_dom = player_ratings_dom.find_all('tr', class_='pncPlayerRow')
+        for player_dom in players_dom:
+            player_id = int(player_dom.find(class_='playertablePlayerName').a['playerid'])
+            ratings_dom = player_dom.find_all('td', class_='playertableData')
+            try:
+                player_rating = float(ratings_dom[-1].string)
+            except ValueError:
+                player_rating = 0 #temporary
+            player_ratings[player_id] = player_rating #TODO: list for rating by each category
 
 class Standings:
     def get_standings(self):
@@ -45,6 +65,7 @@ class Roster:
 
 
 if __name__ == "__main__":
+    build_player_ratings(500)
     r = Roster()
     r.get_rosters(league_id)
     print r.teams
